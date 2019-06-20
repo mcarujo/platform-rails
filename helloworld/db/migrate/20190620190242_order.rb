6208d4f5d283fe2a326758b8a9bc366758eb098c
Marco Carujo <mamcarujo@gmail.com>
2019-06-20 16:37:09 -0300

    rewrite

--- helloworld/db/migrate/20190620190242_order.rb
+++ helloworld/db/migrate/20190620190242_order.rb
@@ -1,16 +1,16 @@
 class Order < ActiveRecord::Migration[5.2]
   def change
-      create_table :orders do |t|
-        t.string :reference
-        t.string :purchaseChannel
-        t.string :clientName
-        t.string :address
-        t.string :deliveryService
-        t.float :totalValue
-        t.text :lineItems
-        t.string :status
+    create_table :orders do |t|
+      t.string :reference
+      t.string :purchaseChannel
+      t.string :clientName
+      t.string :address
+      t.string :deliveryService
+      t.float :totalValue
+      t.text :lineItems
+      t.string :status
 
-        t.timestamps
-      end
+      t.timestamps
+    end
   end
 end
