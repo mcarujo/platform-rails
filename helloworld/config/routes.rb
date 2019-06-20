3d2b6c79cd8c36f9bf9b1d6ecd96128d5dbde170
Marco Carujo <mamcarujo@gmail.com>
2019-06-20 19:09:03 -0300

    new routes added

--- helloworld/config/routes.rb
+++ helloworld/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # The root is a redirect to the orders index
+  root to: "orders#index"
+
+  # orders routes
+  get "order", to: "orders#index" # for see all orders 
+  get "order/:id", to: "orders#show" # for see all orders 
+  post "order", to: "orders#create" # for create a new order
+
+  #batches routes
+  get "batch", to: "batches#index" # for see all batches 
+  get "batch/:id", to: "batches#show" # for see all batches 
+  post "batch", to: "batches#create" # for create a new batch
+
 end
