f8604d28ca58ba966106ebca3f80e42cac763da2
Marco Carujo <mamcarujo@gmail.com>
2019-06-22 22:53:24 -0300

    fix orders id for batch seeds

--- helloworld/db/seeds.rb
+++ helloworld/db/seeds.rb
@@ -15,10 +15,21 @@ end
 50.times do 
     purchaseChannel = Faker::Company.name
     deliveryService = Faker::Company.name
-    batch = Batch.new(reference: Faker::Number.number(4) + '-' + Faker::Number.number(2), purchaseChannel: purchaseChannel, orders: "" )
+    batch = Batch.new(reference: Faker::Number.number(4) + '-' + Faker::Number.number(2), purchaseChannel: purchaseChannel, orders: [] )
+    aux = []
     Faker::Number.between(1,6).times do
-        order = Order.create(reference: "BR" + Faker::Number.number(6), purchaseChannel: purchaseChannel, clientName: Faker::Name.name, address: Faker::Address.full_address, deliveryService: deliveryService, totalValue: Faker::Number.between(1,101), lineItems: Faker::Json.shallow_json(Faker::Number.between(1,6), key: 'Lorem.word', value: 'Lorem.word'),status: [:ready, :production, :closing, :sent].sample )
-        batch.orders << (batch.orders == "" ? order.id.to_s : "-#{order.id.to_s}")
+        order = Order.create(
+                reference: "BR" + Faker::Number.number(6), 
+                purchaseChannel: purchaseChannel, 
+                clientName: Faker::Name.name, 
+                address: Faker::Address.full_address, 
+                deliveryService: deliveryService, 
+                totalValue: Faker::Number.between(1,101), 
+                lineItems: Faker::Json.shallow_json(Faker::Number.between(1,6), key: 'Lorem.word', value: 'Lorem.word'), 
+                status: [:ready, :production, :closing, :sent].sample
+            )
+        aux << order.id
     end
+    batch.orders = aux.to_json
     batch.save
 end
