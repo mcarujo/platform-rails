b9894f2f5fca43c9a568b08285fe217ad7b5830b
Marco Carujo <mamcarujo@gmail.com>
2019-06-22 22:16:52 -0300

    Refactoring controller orders

--- helloworld/app/controllers/orders_controller.rb
+++ helloworld/app/controllers/orders_controller.rb
@@ -2,48 +2,47 @@ class OrdersController < ApplicationController
     include OrdersHelper
 
     def show
-        order = Order.find(params[:id])
-        render json: {status: 'SUCCESS', message:'Order information', data:order},status: :ok
+        render json: {message:'Order information', data: Order.find(params[:id])}, status: :ok
     end
 
     def showAll
-        orders = Order.all
-        render json: {status: 'SUCCESS', message:'Orders list', data:orders},status: :ok
+        render json: {message:'Orders list', data: Order.all},status: :ok
     end
 
     def showStatusByName # Get the status of an Order
-        orders = Order.where(clientName: params[:name]).where.not(status: "sent")
-        if orders.size < 1 or !params[:name]
-            returnMessage = "The name '#{params[:name] ? params[:name] : "invalid" }' has none order"
-        else
-            orders = cutInformationByStatus(orders)
-            returnMessage = 'Orders status by name'
+        if !params.include?(:name) # Validation for name
+            return render json: {message: "No field name", data: false},status: :ok
         end
-        render json: {status: 'SUCCESS', message: returnMessage, data: orders},status: :ok
+        orders = Order.where(clientName: params[:name]).where.not(status: "sent")
+        orders = cutInformationByStatus(orders) # clean the return
+        render json: {message: 'Orders status by name', data: orders},status: :ok
     end
 
     def showListByPurchaseChannel # List the Orders of a Purchase Channel
-        orders = Order.where(purchaseChannel: params[:purchaseChannel], status: params[:status])
-        if orders.size < 1 or !params[:purchaseChannel] or !params[:status]
-            returnMessage = "The purchase channel '#{params[:purchaseChannel] ? params[:purchaseChannel] : "invalid"}' has none order with the status '#{params[:status] ? params[:status] : "invalid"}'"
+        if !params.include?(:purchaseChannel) # Validation for Purchase Channel
+            json = {message: "No field purchaseChannel", data: false}
+        elsif !params.include?(:status) # Validation Status
+            json = {message: "No field status", data: false}
         else
-            returnMessage = 'Order information'
+            orders = Order.where(purchaseChannel: params[:purchaseChannel], status: params[:status])
+            {message: "Orders status by purchase channel and status", data: orders}
         end
-        render json: {status: 'SUCCESS', message: returnMessage, data: orders},status: :ok
+        render json: json, status: :ok
     end
 
     def create # Create a new Order
         postFields = params.permit(:reference, :purchaseChannel, :clientName, :address, :deliveryService, :totalValue, :lineItems, :status)
         order = Order.new postFields
         if order.save
-            render json: {status: 'SUCCESS', message:'Order created', data:order.id}, status: :ok
+           json = {message:'Order created', data: order.id}
         else
-            render json: {status: 'SUCCESS', message: order.errors.full_messages, data: false}, status: :ok
+            json = {message: order.errors.full_messages, data: false}
         end
+        render json: json, status: :ok
     end
     
-    def financialReport # Close part of a Batch for a Delivery Service
+    def financialReport # A simple financial report
         report = Order.select("purchaseChannel, count(id) as quantity, sum(totalValue) as total").where.not(status: 'sents').group('purchaseChannel')
-        render json: {status: 'SUCCESS', message:'Financial Report', data: report}, status: :ok  
+        render json: {message:'Financial Report', data: report}, status: :ok  
     end
 end
