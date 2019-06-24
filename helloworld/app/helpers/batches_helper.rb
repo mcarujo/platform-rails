4147dd1822bd0a0422bfbafa75510beb182d0217
Marco Carujo <mamcarujo@gmail.com>
2019-06-23 22:24:35 -0300

    batch pk control

--- helloworld/app/helpers/batches_helper.rb
+++ helloworld/app/helpers/batches_helper.rb
@@ -1,2 +1,8 @@
 module BatchesHelper
+    def definePKBatches
+        year = Time.now.year
+        month = Time.now.month.to_s.rjust(2, '0')
+        numberBatches = Batch.where("created_at like '#{year}-#{month}%'").size.to_s.rjust(2, '0') # 0X
+        primarykey = year.to_s + month.to_s.to_s.rjust(2, '0') + "-" + numberBatches.to_s
+    end
 end
